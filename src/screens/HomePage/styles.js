8fe6f2668194f483b3c97509f2dd9b66a1e0179c
In Kyu Han <sirasonee@gmail.com>
2020-09-01 19:20:57 -0400

    Implemented Summarizing api

--- src/screens/HomePage/styles.js
+++ src/screens/HomePage/styles.js
@@ -1,5 +1,5 @@
-
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
 
 export default StyleSheet.create({
     container: {
@@ -16,5 +16,25 @@ export default StyleSheet.create({
         marginLeft: 30,
         marginRight: 30,
         paddingLeft: 16
+    },
+    title: {
+    		color: '#FFF',
+    		marginTop: 10,
+    		textAlign: 'center'
+    },
+    buttonsContainer: {
+    		padding: 25,
+    		width: 375
+    },
+    buttonText: {
+    		textAlign: 'center',
+    		color: '#FFF',
+    		fontWeight: '800'
+    },
+    buttonContainer: {
+    		backgroundColor: '#16a085',
+    		paddingVertical: 10,
+    		marginBottom: 10,
+    		marginTop: 10
     }
 })
