64b618fd30836a8f521245480832e721b3288a73
Gao David <aqdajz@gmail.com>
2020-09-02 10:48:57 -0400

    first submission

--- src/screens/HomePage/HomePage.js
+++ src/screens/HomePage/HomePage.js
@@ -1,61 +1,10 @@
 import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import {firebase} from '../../firebase/config';
-import * as FileSystem from 'expo-file-system';
-import * as DocumentPicker from 'expo-document-picker';
-
-const storageRef = firebase.storage().ref();
-const AylientextAPI = require('aylien_textapi'); //Need to perform npm install aylien_textapi//
-const textapi = new AylientextAPI({
-    application_id: '12049eaa'
-    application_key: '0050eee15d54ac3dbd69d868fc1e8570'
-})
-
+import { Text, View } from 'react-native'
+z
 export default function HomeScreen(props) {
-
-    constructor() {
-        super();
-        this.state = {
-            file: null,
-            fileuri: ''
-        }
-    }
-
-    pickDocument = async () => {
-        const result = await DocumentPicker.getDocumentAsync({});
-        console.log('result', result);
-        if (!result.cancelled) {
-            this.setState({
-                file: result,
-                fileuri: result.uri
-            });
-        }
-    }
-
-    summarizeNow = async () => {
-        textapi.summarize({
-            url: this.state.fileuri,
-            sentences_number: 5
-        }, function(error, response) {
-            if (error === null) {
-                response.sentences.forEach(function(s) {
-                    console.log(s);
-                });
-            }
-        });
-    }
-
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Home Screen</Text>
-        </View>
-        <View style={styles.buttonsContainer}>
-            <TouchableOpacity onPress={() => this.pickDocument()} style={styles.buttonContainer}>
-                <Text style={styles.buttonText}>Choose File</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => this.summarizeNow()} style={styles.buttonContainer}>
-                <Text style={styles.buttonText}>Summarize</Text>
-            </TouchableOpacity>
+        <View>
+            <Text>Home Screen</Text>
         </View>
     )
 }
--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -3,49 +3,39 @@ import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from './styles';
 
-import {firebase} from '../../firebase/config';
-
 export default function LoginScreen({navigation}) {
-    constructor() {
-        super();
-        this.state = {
-            email: '',
-            password: '',
-            isLoading: false
-        }
-    }
-
-    setState = (val, prop) => {
-        const state = this.state;
-        state[prop] = val;
-        this.setState(state);
-    }
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
     const onFooterLinkPress = () => {
         navigation.navigate('Registration')
     }
 
     const onLoginPress = () => {
-        if(this.state.email == '' && this.state.password == '') {
-            Alert.alert('Either email or password is missing!')
-        } else {
-            this.setState({
-                isLoading: true,
-            })
-            firebase
+        firebase
             .auth()
-            .signInWithEmailAndPassword(this.state.email, this.state.password)
-            .then((res) => {
-                console.log(res)
-                console.log('User logged in successfully')
-                this.setState({
-                    email: '',
-                    password: ''
-                    isLoading: false
-                })
+            .signInWithEmailAndPassword(email, password)
+            .then((response) => {
+                const uid = response.user.uid
+                const usersRef = firebase.firestore().collection('users')
+                usersRef
+                    .doc(uid)
+                    .get()
+                    .then(firestoreDocument => {
+                        if (!firestoreDocument.exists) {
+                            alert("User does not exist anymore.")
+                            return;
+                        }
+                        const user = firestoreDocument.data()
+                        navigation.navigate('Home', {user})
+                    })
+                    .catch(error => {
+                        alert(error)
+                    });
             })
-            .catch(error => this.setState({ errorMessage: error.message}))
-        }
+            .catch(error => {
+                alert(error)
+            })
     }
 
     return (
@@ -61,8 +51,8 @@ export default function LoginScreen({navigation}) {
                     style={styles.input}
                     placeholder='E-mail'
                     placeholderTextColor="#aaaaaa"
-                    value={this.state.email}
-                    onChangeText={(val) => this.updateInputVal(val, 'email')}
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
                     underlineColorAndroid="transparent"
                     autoCapitalize="none"
                 />
@@ -71,8 +61,8 @@ export default function LoginScreen({navigation}) {
                     placeholderTextColor="#aaaaaa"
                     secureTextEntry
                     placeholder='Password'
-                    value={this.state.password}
-                    onChangeText={(val) => this.updateInputVal(val, 'password')}
+                    onChangeText={(text) => setPassword(text)}
+                    value={password}
                     underlineColorAndroid="transparent"
                     autoCapitalize="none"
                 />
